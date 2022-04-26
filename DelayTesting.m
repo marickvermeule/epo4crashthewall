2317556d51c3e4c95065131405055166393484c8
ahirsig <66413175+ahirsig@users.noreply.github.com>
2022-04-26 15:06:28 +0200

    To test the delay and frequency of the bluetooth

---
+++ DelayTesting.m
@@ -0,0 +1,20 @@
+tic
+status = EPOCommunication('transmit', S)
+disp(status)
+status = EPOCommunication('transmit', S)
+toc
+% first - second = bluetootb frquency
+tic
+status = EPOCommunication('transmit', S)
+toc
+
+
+tic
+status = EPOCommunication('transmit', S)
+disp(status)
+toc
+% third - fourth = delay
+tic
+disp(status)
+toc
+
